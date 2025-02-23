cd616165a2554a78f7064e27c6f1f8c59837477c
Dan Siqueira <dansiqueira0@gmail.com>
2025-02-23 13:43:05 -0300

    Create TaskController

--- src/main/java/com/danielsonsiqueira/todosimple/services/TaskService.java
+++ src/main/java/com/danielsonsiqueira/todosimple/services/TaskService.java
@@ -4,10 +4,13 @@ import com.danielsonsiqueira.todosimple.models.Task;
 import com.danielsonsiqueira.todosimple.models.User;
 import com.danielsonsiqueira.todosimple.repositories.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TaskService {
 
     @Autowired
@@ -22,6 +25,11 @@ public class TaskService {
                 "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName())
         );
     }
+    public List<Task> findAllByUserId (Long userId){
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
+    }
+
     @Transactional
     public Task create(Task obj){
         User user = this.userService.findById(obj.getUser().getId());
