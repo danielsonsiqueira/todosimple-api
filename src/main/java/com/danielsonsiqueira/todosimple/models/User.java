6259f19117d08729e2c1cdef788831ece2517ec9
Dan Siqueira <dansiqueira0@gmail.com>
2025-02-23 11:35:58 -0300

    Create UserController

--- src/main/java/com/danielsonsiqueira/todosimple/models/User.java
+++ src/main/java/com/danielsonsiqueira/todosimple/models/User.java
@@ -1,5 +1,6 @@
 package com.danielsonsiqueira.todosimple.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
@@ -55,6 +56,7 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Task> tasks = new ArrayList<Task>();
 
+    @JsonIgnore
     public List<Task> getTasks() {
         return tasks;
     }
