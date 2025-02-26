c5bb8df5a508a9b4a5e3a69990a78a04aaea2545
Dan Siqueira <dansiqueira0@gmail.com>
2025-02-25 21:42:49 -0300

    Add Lombok

--- src/main/java/com/danielsonsiqueira/todosimple/models/User.java
+++ src/main/java/com/danielsonsiqueira/todosimple/models/User.java
@@ -1,15 +1,14 @@
 package com.danielsonsiqueira.todosimple.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
 
@@ -23,6 +22,11 @@ import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
 */
 @Entity
 @Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
 
     public interface CreateUser{}
@@ -54,80 +58,7 @@ public class User {
 
     */
     @OneToMany(mappedBy = "user")
+    @JsonProperty(access = WRITE_ONLY)
     private List<Task> tasks = new ArrayList<Task>();
-
-    @JsonIgnore
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-
-    public User() {
-    }
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    //verifica se o objeto é igual em todos os casos abaixo (garantia de que é o mesmo usuario)
-    public boolean equals(Object obj){
-
-        if(obj == this)
-            return true;
-        if(obj == null)
-            return false;
-        if(!(obj instanceof User))
-            return false;
-
-        User other = (User) obj;
-
-        if(this.id == null)
-            if(other.id != null)
-                return false;
-        else if(!this.id.equals(other.id))
-            return false;
-        return Objects.equals(this.id, other.id)
-                && Objects.equals(this.username, other.username)
-                && Objects.equals(this.password, other.password);
-
-    }
-
-    //verificador de valor unico para o id
-    public int hashCode(){
-
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-        return result;
-    }
 
 }
